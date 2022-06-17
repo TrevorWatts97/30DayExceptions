ba4319db56f0eb0a0cff1110136841d456bbcf2f
Trevor Watts <60370743+TrevorWatts97@users.noreply.github.com>
2022-06-17 12:51:26 -0400

    Add files via upload

---
+++ 30DayException.ps1
@@ -0,0 +1,21 @@
+﻿#Paste this install-module into another powershell terminal if you need to
+#Install-Module -Name ImportExcel
+
+Import-Module ActiveDirectory
+Import-Module ImportExcel
+
+$group = "30dayExceptionTest"
+
+#Clear current members
+Get-ADGroupMember $group | ForEach-Object {Remove-ADGroupMember -Identity $group $_ -Confirm:$false}
+
+#Import file
+$filename = "C:\Scripts\30DayExceptions.xlsx"
+$Users = Import-Excel $filename
+
+#Add each user into AD Group
+foreach($User in $Users)
+{ 
+    $Sam = $User.UserPrincipalName -replace ".{10}$"
+    Add-ADGroupMember -Identity $group -Members $Sam
+}
